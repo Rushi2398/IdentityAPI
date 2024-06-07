8634760f1812208bd98678c5993fa9a2dbc74ae0
Rushikesh Jalvi <rushikeshjalvi23@gmail.com>
2024-06-07 21:46:29 +0530

    Added deployment file and code refactoring

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
+import { Response } from "express";
 
 const prisma = new PrismaClient();
 
@@ -31,6 +31,7 @@ export const getAllContacts = async (email: string | null | undefined, phoneNumb
     const uniqueLinkIds = new Set<number>();
 
     initialContacts.forEach(contact => {
+
         allContacts.push(contact);
         if (contact.linkedId) uniqueLinkIds.add(contact.linkedId);
     });
@@ -53,15 +54,16 @@ export const getAllContacts = async (email: string | null | undefined, phoneNumb
                     where: { id: contact.linkedId }
                 });
             }
-            const otherContacts = await prisma.contact.findMany({
-                where: {
-                    linkedId: contact?.id
-                }
-            })
-            allContacts.push(...otherContacts);
         }
     }
-
+    for (const contact of initialContacts) {
+        const otherContacts = await prisma.contact.findMany({
+            where: {
+                linkedId: contact?.id
+            }
+        })
+        allContacts.push(...otherContacts);
+    }
     return allContacts;
 }
 
@@ -79,7 +81,7 @@ export const createPrimaryContact = async (email: string | null | undefined, pho
 }
 
 // Create a secondary contact since the email or phoneNumber is already present with a primary contact
-export const createSecondaryContact = async (email: string | null | undefined, phoneNumber: string | null | undefined, primaryContact: Contact) => {
+const createSecondaryContact = async (email: string | null | undefined, phoneNumber: string | null | undefined, primaryContact: Contact) => {
     const newSecondaryContact = await prisma.contact.create({
         data: {
             email: email ?? primaryContact.email,
@@ -97,7 +99,7 @@ export const createSecondaryContact = async (email: string | null | undefined, p
 }
 
 // Update all the contacts whose primary Id is converted to Secondary Id
-export const updateSecondaryContact = async (email: string | null | undefined, phoneNumber: string | null | undefined, allContacts: Contact[]) => {
+const updateSecondaryContact = async (email: string | null | undefined, phoneNumber: string | null | undefined, allContacts: Contact[]) => {
     const primaryContacts = allContacts.filter(contact => contact.linkedId === null);
     let oldContact, newContact;
 
@@ -130,5 +132,55 @@ export const updateSecondaryContact = async (email: string | null | undefined, p
     });
 
     return updatedContacts;
+}
+
+export const returnResult = async (email: string | null, phoneNumber: string | null, result: Contact[], res: Response) => {
+    const primaryContacts = result.filter(contact => contact.linkedId === null);
+    let primaryId = getUniqueContacts(primaryContacts);
+    let contactExist = result.some(contact => contact.email === email && contact.phoneNumber === phoneNumber);
+
+    contactExist = email && phoneNumber ? contactExist : true;
+
+    // Case 1: Where there is only one primary Contact for the given email and phoneNumber
+    if (!contactExist && primaryId.length === 1) {
+        const secondaryContact = await createSecondaryContact(email, phoneNumber, primaryId[0]);
+        result.push(secondaryContact);
+    }
+
+    // Case 2: Where there are 2 primary Contacts for the given email and phoneNumber.
+    if (!contactExist && primaryId.length === 2) {
+        const updatedContacts = await updateSecondaryContact(email, phoneNumber, result);
+        result = await getAllContacts(email, phoneNumber);
+        result.push(...updatedContacts)
+    }
+
+    // return the required result in JSON format 
+    primaryId = result.filter(contact => contact.linkedId === null);
+
+    const emailIds = new Set(result.map(contact => contact.email));
+    const phones = new Set(result.map(contact => contact.phoneNumber));
+    const secondaryIds = new Set(result.filter(contact => contact.id !== primaryId[0].id).map(contact => contact.id));
+
+    return res.status(200).json({
+        contact: {
+            primaryContactId: primaryId[0].id,
+            emails: Array.from(emailIds),
+            phoneNumbers: Array.from(phones),
+            secondaryContactIds: Array.from(secondaryIds)
+        }
+    });
+}
+
+//get Unique Contacts (objects) from the PrimaryContacts: Remove repeated contacts
+const getUniqueContacts = (contacts: Contact[]): Contact[] => {
+    const uniqueContactsMap = new Map<number, Contact>();
+
+    contacts.forEach(contact => {
+        if (!uniqueContactsMap.has(contact.id)) {
+            uniqueContactsMap.set(contact.id, contact);
+        }
+    });
+
+    return Array.from(uniqueContactsMap.values());
 }
 
